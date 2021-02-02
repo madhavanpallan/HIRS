90ebd4c902a22f233a6072efff3ae2d369b873e1
Cyrus <24922493+cyrus-dev@users.noreply.github.com>
2021-02-02 14:49:28 -0500

    Updated the parameters of the child class because the super implementation was updated.

--- HIRS_Utils/src/main/java/hirs/data/persist/type/X509CertificateType.java
+++ HIRS_Utils/src/main/java/hirs/data/persist/type/X509CertificateType.java
@@ -18,6 +18,7 @@ import javax.sql.rowset.serial.SerialBlob;
 
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.usertype.UserType;
 
 
@@ -100,7 +101,7 @@ public final class X509CertificateType implements UserType {
      */
     @Override
     public Object nullSafeGet(final ResultSet rs, final String[] names,
-            final SessionImplementor session, final Object owner)
+            final SharedSessionContractImplementor session, final Object owner)
             throws HibernateException, SQLException {
         final Blob cert = rs.getBlob(names[0]);
         if (cert == null) {
@@ -130,7 +131,7 @@ public final class X509CertificateType implements UserType {
      */
     @Override
     public void nullSafeSet(final PreparedStatement st, final Object value,
-            final int index, final SessionImplementor session)
+            final int index, final SharedSessionContractImplementor session)
             throws SQLException {
         if (value == null) {
             st.setString(index, null);
